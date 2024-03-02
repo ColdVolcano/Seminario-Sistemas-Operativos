91af16d9e52bd288f456df229c7240abcf0ac6f5
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2024-03-01 21:44:03 -0600

    escape everything?
    apparently space chars are a thing (we do not like freedom of speech)

--- Challenge 1/vm-configure.sh
+++ Challenge 1/vm-configure.sh
@@ -2,13 +2,13 @@
 
 # Assume config is gotten here
 
-VBoxManage createvm --name "$NAME" --ostype "$OSTYPE" --register
-VBoxManage modifyvm "$NAME" --ioapic on
-VBoxManage modifyvm "$NAME" --memory $MEMSIZE --vram $VRAMSIZE
-VBoxManage createhd --filename "$PWD/$NAME/$NAME.vhd" --size $VDDSIZE --format VHD
-VBoxManage storagectl "$NAME" --name "$SATACONNAME" --add sata --controller IntelAhci
-VBoxManage storageattach "$NAME" --storagectl "$SATACONNAME" --port 0 --device 0 --type hdd --medium "$PWD/$NAME/$NAME.vhd"
-VBoxManage storagectl "$NAME" --name "$IDECONNAME" -add ide --controller PIIX4
-VBoxManage storageattach "$NAME" --storagectl "$IDECONNAME" --port 1 --device 0 --type dvddrive
+VBoxManage createvm --name "\"$NAME\"" --ostype "$OSTYPE" --register
+VBoxManage modifyvm "\"$NAME\"" --ioapic on
+VBoxManage modifyvm "\"$NAME\"" --memory $MEMSIZE --vram $VRAMSIZE
+VBoxManage createhd --filename "\"$PWD/$NAME/$NAME.vhd\"" --size $VDDSIZE --format VHD
+VBoxManage storagectl "\"$NAME\"" --name "\"$SATACONNAME\"" --add sata --controller IntelAhci
+VBoxManage storageattach "\"$NAME\"" --storagectl "\"$SATACONNAME\"" --port 0 --device 0 --type hdd --medium "\"$PWD/$NAME/$NAME.vhd\""
+VBoxManage storagectl "\"$NAME\"" --name "\"$IDECONNAME\"" -add ide --controller PIIX4
+VBoxManage storageattach "\"$NAME\"" --storagectl "\"$IDECONNAME\"" --port 1 --device 0 --type dvddrive
 
-VBoxManage showvminfo "$NAME"
+VBoxManage showvminfo "\"$NAME\""

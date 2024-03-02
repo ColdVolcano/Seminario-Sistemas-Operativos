0d3f3d00929c14680d6d90942e856ff8170c0c18
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2024-03-01 21:36:21 -0600

    change disk type to vhd

--- Challenge 1/vm-configure.sh
+++ Challenge 1/vm-configure.sh
@@ -5,9 +5,9 @@
 VBoxManage createvm --name "$NAME" --ostype "$OSTYPE" --register
 VBoxManage modifyvm "$NAME" --ioapic on
 VBoxManage modifyvm "$NAME" --memory $MEMSIZE --vram $VRAMSIZE
-VBoxManage createhd --filename "$PWD/$NAME/$NAME-vdd.vdi" --size $VDDSIZE --format VDI
+VBoxManage createhd --filename "$PWD/$NAME/$NAME.vhd" --size $VDDSIZE --format VHD
 VBoxManage storagectl "$NAME" --name "$SATACONNAME" --add sata --controller IntelAhci
-VBoxManage storageattach "$NAME" --storagectl "$SATACONNAME" --port 0 --device 0 --type hdd --medium "$PWD/$NAME/$NAME-vdd.vdi"
+VBoxManage storageattach "$NAME" --storagectl "$SATACONNAME" --port 0 --device 0 --type hdd --medium "$PWD/$NAME/$NAME.vhd"
 VBoxManage storagectl "$NAME" --name "$IDECONNAME" -add ide --controller PIIX4
 VBoxManage storageattach "$NAME" --storagectl "$IDECONNAME" --port 1 --device 0 --type dvddrive
 

e93a32c54bd7c5adeb5a2faebd4c95cc271f1728
ColdVolcano <16726733+ColdVolcano@users.noreply.github.com>
2024-03-08 22:55:33 -0600

    actually add the menu

--- Challenge 1/vm-configure.sh
+++ Challenge 1/vm-configure.sh
@@ -1,12 +1,29 @@
 #!/usr/bin/bash
 
+OPTSTRING="n:o:c:r:v:d:s:i:h"
+
+while getopts ${OPTSTRING} option;
+do
+  case "${option}" in
+    n) NAME=${OPTARG};;
+    o) OSTYPE=${OPTARG};;
+    c) CPUCOUNT=${OPTARG};;
+    r) MEMSIZE=${OPTARG};;
+    v) VRAMSIZE=${OPTARG};;
+    d) DISKNAME=${OPTARG};;
+    s) SATACONNAME=${OPTARG};;
+    i) IDECONNAME=${OPTARG};;
+      exit ;;
+  esac
+done
+
 VBoxManage createvm --name $NAME --ostype $OSTYPE --register
 VBoxManage modifyvm $NAME --ioapic on
-VBoxManage modifyvm $NAME --memory $MEMSIZE
-VBoxManage modifyvm $NAME --vram $VRAMSIZE
-VBoxManage createhd --filename $PWD/$NAME/$NAME.vhd --size $VDDSIZE --format VHD
+VBoxManage modifyvm $NAME --cpus $CPUCOUNT
+VBoxManage modifyvm $NAME --memory $MEMSIZE --vram $VRAMSIZE
+VBoxManage createhd --filename $PWD/$NAME/$DISKNAME.vhd --size $VDDSIZE --format VHD
 VBoxManage storagectl $NAME --name $SATACONNAME --add sata --controller IntelAhci
-VBoxManage storageattach $NAME --storagectl $SATACONNAME --port 0 --device 0 --type hdd --medium $PWD/$NAME/$NAME.vhd
+VBoxManage storageattach $NAME --storagectl $SATACONNAME --port 0 --device 0 --type hdd --medium $PWD/$NAME/$DISKNAME.vhd
 VBoxManage storagectl $NAME --name "\'$IDECONNAME\'" -add ide --controller PIIX4
 VBoxManage storageattach $NAME --storagectl $IDECONNAME --port 1 --device 0 --type dvddrive
 
